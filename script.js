f7c4e63261e41b4fb8d76cd8d71d8e2229881566
quangvu237-cyber <quangvu2372k5@gmail.com>
2025-11-17 13:44:57 +0700

    my_profile

---
+++ script.js
@@ -0,0 +1,14 @@
+// // Bạn cần đặt đoạn mã này trong một tệp .js và liên kết nó trong HTML
+// document.querySelectorAll('.main-nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+        
+//         // Cập nhật trạng thái active
+//         document.querySelectorAll('.main-nav a').forEach(link => link.classList.remove('active'));
+//         this.classList.add('active');
+//     });
+// }) 
